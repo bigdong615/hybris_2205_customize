a1abc45aa04612b83853fa7089011a586708378a
vijayvv7060 <vvishwakarma@dminc.com>
2021-03-23 13:57:13 +0530

    [BL-24] : Changes related to login and signout page.

--- bin/custom/blstorefront/web/src/com/bl/storefront/security/LoginAuthenticationFailureHandler.java
+++ bin/custom/blstorefront/web/src/com/bl/storefront/security/LoginAuthenticationFailureHandler.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.security.core.AuthenticationException;
@@ -19,16 +20,39 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationFa
 public class LoginAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler
 {
 	private BruteForceAttackCounter bruteForceAttackCounter;
+	private boolean useReferer = false;
+	private boolean checkoutLogin = false;
 
 	@Override
 	public void onAuthenticationFailure(final HttpServletRequest request, final HttpServletResponse response,
 			final AuthenticationException exception) throws IOException, ServletException
 	{
+		String targetUrl = request.getHeader("Referer");
 		// Register brute attacks
 		bruteForceAttackCounter.registerLoginFailure(request.getParameter("j_username"));
 
 		// Store the j_username in the session
 		request.getSession().setAttribute("SPRING_SECURITY_LAST_USERNAME", request.getParameter("j_username"));
+
+		HttpSession session = request.getSession(false);
+
+		if (session != null || isAllowSessionCreation())
+		{
+			
+			if (targetUrl == null) {
+				super.setDefaultFailureUrl("/");
+			} else {
+				super.setDefaultFailureUrl(targetUrl);
+			}
+		}
+		if(this.useReferer && checkoutLogin)
+		{
+			if (targetUrl == null) {
+				super.setDefaultFailureUrl("/");
+			} else {
+				super.setDefaultFailureUrl(targetUrl);
+			}
+		}
 
 		super.onAuthenticationFailure(request, response, exception);
 	}
@@ -45,4 +69,29 @@ public class LoginAuthenticationFailureHandler extends SimpleUrlAuthenticationFa
 	{
 		this.bruteForceAttackCounter = bruteForceAttackCounter;
 	}
+
+
+
+	public boolean isUseReferer() {
+		return useReferer;
+	}
+
+
+
+	public void setUseReferer(boolean useReferer) {
+		this.useReferer = useReferer;
+	}
+
+
+
+	public boolean isCheckoutLogin() {
+		return checkoutLogin;
+	}
+
+
+
+	public void setCheckoutLogin(boolean checkoutLogin) {
+		this.checkoutLogin = checkoutLogin;
+	}
+
 }
