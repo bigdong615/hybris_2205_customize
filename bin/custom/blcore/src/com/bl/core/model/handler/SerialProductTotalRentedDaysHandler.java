fe9b5e5736eaa69f94944b4d9b1f080eecce4ddc
ritika_pragiti7 <rkohli@dminc.com>
2021-03-26 16:04:11 +0530

    [BL-91]: 1) Updated product and category impexes 2) Added new attributes on BlProduct and BlSerailProducts 3) Added Serial data and recommndation data impex 4) Added backoffice configuration

---
+++ bin/custom/blcore/src/com/bl/core/model/handler/SerialProductTotalRentedDaysHandler.java
@@ -0,0 +1,22 @@
+package com.bl.core.model.handler;
+
+import com.bl.core.model.BlSerialProductModel;
+import de.hybris.platform.servicelayer.model.attribute.DynamicAttributeHandler;
+
+/**
+ * TODO: The implementation for the class will be done later
+ *
+ * @author Ritika
+ */
+public class SerialProductTotalRentedDaysHandler implements DynamicAttributeHandler<Long, BlSerialProductModel> {
+
+  @Override
+  public Long get(BlSerialProductModel model) {
+    return null;
+  }
+
+  @Override
+  public void set(BlSerialProductModel model, Long aLong) {
+
+  }
+}
---
+++ bin/custom/blcore/src/com/bl/core/model/handler/SerialProductTotalRepairsHandler.java
@@ -0,0 +1,22 @@
+package com.bl.core.model.handler;
+
+import com.bl.core.model.BlSerialProductModel;
+import de.hybris.platform.servicelayer.model.attribute.DynamicAttributeHandler;
+
+/**
+ * TODO: The implementation for the class will be done later
+ *
+ * @author Ritika
+ */
+public class SerialProductTotalRepairsHandler implements DynamicAttributeHandler<Double, BlSerialProductModel> {
+
+  @Override
+  public Double get(BlSerialProductModel model) {
+    return null;
+  }
+
+  @Override
+  public void set(BlSerialProductModel model, Double aDouble) {
+
+  }
+}
