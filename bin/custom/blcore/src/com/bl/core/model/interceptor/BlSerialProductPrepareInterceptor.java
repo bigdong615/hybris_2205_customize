35189abe0cd8c464c5e86737e57702528c23b215
ritika_pragiti7 <rkohli@dminc.com>
2021-04-07 20:34:17 +0530

    [Bl-480]: Added changes for Serial price forSale && incentivizedPrice

--- bin/custom/blcore/src/com/bl/core/model/interceptor/BlSerialProductPrepareInterceptor.java
+++ bin/custom/blcore/src/com/bl/core/model/interceptor/BlSerialProductPrepareInterceptor.java
@@ -6,6 +6,8 @@ import de.hybris.platform.servicelayer.interceptor.InterceptorContext;
 import de.hybris.platform.servicelayer.interceptor.InterceptorException;
 import de.hybris.platform.servicelayer.interceptor.PrepareInterceptor;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import org.apache.commons.lang.BooleanUtils;
 
 public class BlSerialProductPrepareInterceptor implements PrepareInterceptor<BlSerialProductModel> {
 
@@ -13,14 +15,40 @@ public class BlSerialProductPrepareInterceptor implements PrepareInterceptor<BlS
 
   @Override
   public void onPrepare(final BlSerialProductModel blSerialProduct,final InterceptorContext ctx) throws InterceptorException {
-    if(ctx.isNew(blSerialProduct) && hasForSaleBaseAndConditionalRating(blSerialProduct)){
-        blSerialProduct.setFinalSalePrice(getBlPricingService().calculateSerialForSalePrice(blSerialProduct.getForSaleBasePrice(),blSerialProduct.getConditionRatingOverallScore()));
-    }
-    if( blSerialProduct.getFinalSalePrice() == null || ctx.isModified(blSerialProduct,BlSerialProductModel.FORSALEBASEPRICE) || ctx.isModified(blSerialProduct,BlSerialProductModel.CONDITIONRATINGOVERALLSCORE) && hasForSaleBaseAndConditionalRating(blSerialProduct))
-    {
-      blSerialProduct.setFinalSalePrice(getBlPricingService().calculateSerialForSalePrice(blSerialProduct.getForSaleBasePrice(),blSerialProduct.getConditionRatingOverallScore()));
+    calculateForSalePriceForSerial(blSerialProduct,ctx);
+    calculateIncentivizedPriceForSerial(blSerialProduct,ctx);
+  }
+
+  /**
+   *
+   * @param blSerialProduct
+   * @param ctx
+   */
+  private void updateForSalePriceForSerial(final BlSerialProductModel blSerialProduct, final InterceptorContext ctx) {
+    if (BooleanUtils.isTrue(blSerialProduct.getForSale()) && hasForSaleBaseAndConditionalRating(blSerialProduct)) {
+     if (ctx.isNew(blSerialProduct) || blSerialProduct.getFinalSalePrice() == null || ctx.isModified(blSerialProduct, BlSerialProductModel.FORSALEBASEPRICE)
+          || ctx.isModified(blSerialProduct, BlSerialProductModel.CONDITIONRATINGOVERALLSCORE)) {
+        blSerialProduct.setFinalSalePrice(getBlPricingService().calculateSerialForSalePrice(blSerialProduct.getForSaleBasePrice(),
+                blSerialProduct.getConditionRatingOverallScore()));
+      }
     }
   }
+
+  /**
+   * Calculate incentivized price based
+   * on the finalSalePrice
+   * @param blSerialProduct
+   * @param ctx
+   */
+  private void calculateIncentivizedPriceForSerial(BlSerialProductModel blSerialProduct, InterceptorContext ctx) {
+    final BigDecimal finalSalePrice = blSerialProduct.getFinalSalePrice().setScale(2, RoundingMode.DOWN);
+    final Integer forSaleDiscount = blSerialProduct.getForSaleDiscount();
+    if( finalSalePrice.compareTo(BigDecimal.ZERO) > 0 && forSaleDiscount > 0 && (blSerialProduct.getIncentivizedPrice().compareTo(BigDecimal.ZERO) == 0 || ctx
+        .isModified(blSerialProduct,BlSerialProductModel.FINALSALEPRICE) || ctx.isModified(blSerialProduct,BlSerialProductModel.FORSALEDISCOUNT))){
+      blSerialProduct.setIncentivizedPrice(finalSalePrice.multiply(new BigDecimal(forSaleDiscount)).divide(new BigDecimal(100)).setScale(2,RoundingMode.DOWN));
+    }
+  }
+
 
   /**
    * Check if the sale price is not null and greater than zero
@@ -29,7 +57,7 @@ public class BlSerialProductPrepareInterceptor implements PrepareInterceptor<BlS
    * @return
    */
   private boolean hasForSaleBaseAndConditionalRating(final BlSerialProductModel blSerialProduct) {
-    return blSerialProduct.getForSaleBasePrice().compareTo(BigDecimal.ZERO) != 0  &&  blSerialProduct.getConditionRatingOverallScore() > 0.0D;
+    return blSerialProduct.getForSaleBasePrice().compareTo(BigDecimal.ZERO) > 0  &&  blSerialProduct.getConditionRatingOverallScore() > 0.0D;
   }
 
   public BlPricingService getBlPricingService() {
