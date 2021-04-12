2a3df42454fd4623e528e6fadc30a7aa29b1770b
sahanasb <sahanab@pragiti.com>
2021-04-12 18:44:54 +0530

    [feature/BL-174: made some chnages on test class]

--- bin/custom/blfacades/testsrc/com/bl/facades/product/populator/BlProductTagPopulatorTest.java
+++ bin/custom/blfacades/testsrc/com/bl/facades/product/populator/BlProductTagPopulatorTest.java
@@ -25,13 +25,6 @@ public class BlProductTagPopulatorTest {
   @InjectMocks
   private final BlProductTagPopulator populator = Mockito.spy(new BlProductTagPopulator());
 
-
-  public static String PRODUCT_NEW = "new";
-  public static String PRODUCT_MOSTPOPULAR = "MOSTPOPULAR";
-  public static String PRODUCT_GREATVALUE = "GREATVALUE";
-  public static String PRODUCT_STAFFPICK = "STAFFPICK";
-  public static String PRODUCT_FORRENT = "PRODUCT_FORRENT";
-
   @Mock
   BlProductModel productModel;
   @Mock
@@ -46,25 +39,34 @@ public class BlProductTagPopulatorTest {
 
   @Test
   public void populateProductTagPopularDetails() {
-    when(productModel.getIsNew()).thenReturn(Boolean.TRUE);
-    when(productModel.getMostPopular()).thenReturn(Boolean.TRUE);
-    when(productModel.getForRent()).thenReturn(Boolean.TRUE);
-    when(productModel.getGreatValue()).thenReturn(Boolean.TRUE);
-
-    populator.populate(productModel, productData);
-    Assert.assertEquals(productData.getProductTagValues(), BlCoreConstants.NEW);
-    Assert.assertNotEquals(productData.getProductTagValues(), BlCoreConstants.POPULAR);
-    Assert.assertNotEquals(productData.getProductTagValues(), BlCoreConstants.GREAT_VALUE);
-  }
-
-  @Test
-  public void populateProductTag1PopularDetails() {
     when(productModel.getIsNew()).thenReturn(Boolean.FALSE);
     when(productModel.getMostPopular()).thenReturn(Boolean.TRUE);
-
     populator.populate(productModel, productData);
     Assert.assertNotEquals(productData.getProductTagValues(), BlCoreConstants.NEW);
     Assert.assertEquals(productData.getProductTagValues(), BlCoreConstants.POPULAR);
+  }
+
+  @Test
+  public void populateProductTagRentDetails() {
+    when(productModel.getIsNew()).thenReturn(Boolean.FALSE);
+    when(productModel.getMostPopular()).thenReturn(Boolean.FALSE);
+    when(productModel.getForRent()).thenReturn(Boolean.TRUE);
+    when(productModel.getGreatValue()).thenReturn(Boolean.TRUE);
+    populator.populate(productModel, productData);
+    Assert.assertNotEquals(productData.getProductTagValues(), BlCoreConstants.NEW);
+    Assert.assertEquals(productData.getProductTagValues(), BlCoreConstants.GREAT_VALUE);
+  }
+
+  @Test
+  public void populateProductTagStaffDetails() {
+    when(productModel.getIsNew()).thenReturn(Boolean.FALSE);
+    when(productModel.getMostPopular()).thenReturn(Boolean.FALSE);
+    when(productModel.getForRent()).thenReturn(Boolean.TRUE);
+    when(productModel.getGreatValue()).thenReturn(Boolean.FALSE);
+    when(productModel.getStaffPick()).thenReturn(Boolean.TRUE);
+    populator.populate(productModel, productData);
+    Assert.assertNotEquals(productData.getProductTagValues(), BlCoreConstants.NEW);
+    Assert.assertEquals(productData.getProductTagValues(), BlCoreConstants.STAFF_PICK);
   }
 }
 
