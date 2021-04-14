e8ddd3c9cbe3cd0ac2dacff21a450d5b151c1151
sahanasb <sahanab@pragiti.com>
2021-04-14 16:21:23 +0530

    [js issue on the server fixed and written test class for the populator]

--- bin/custom/blfacades/testsrc/com/bl/facades/product/populator/BlProductTagPopulatorTest.java
+++ bin/custom/blfacades/testsrc/com/bl/facades/product/populator/BlProductTagPopulatorTest.java
@@ -8,13 +8,18 @@ import de.hybris.platform.commercefacades.product.data.ProductData;
 import de.hybris.platform.core.model.product.ProductModel;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -27,23 +32,40 @@ public class BlProductTagPopulatorTest {
 
   @Mock
   BlProductModel productModel;
-  @Mock
+
   ProductData productData;
 
+  @Before
+  public void prepare(){
+    MockitoAnnotations.initMocks(this);
+    productData = new ProductData();
+  }
   @Test
   public void populateProductTagNewDetails() {
     when(productModel.getIsNew()).thenReturn(Boolean.TRUE);
+    when(productModel.getMostPopular()).thenReturn(Boolean.TRUE);
+    when(productModel.getForRent()).thenReturn(Boolean.TRUE);
+    when(productModel.getGreatValue()).thenReturn(Boolean.TRUE);
+    when(productModel.getStaffPick()).thenReturn(Boolean.TRUE);
     populator.populate(productModel, productData);
-    Assert.assertEquals(productData.getProductTagValues(), BlCoreConstants.NEW);
+    Assert.assertEquals( BlCoreConstants.NEW ,productData.getProductTagValues());
+    assertNotEquals(productData.getProductTagValues(),BlCoreConstants.POPULAR);
+    assertNotEquals(productData.getProductTagValues(),BlCoreConstants.GREAT_VALUE_STRING);
+    assertNotEquals(productData.getProductTagValues(),BlCoreConstants.STAFF_PICK_STRING);
   }
 
   @Test
   public void populateProductTagPopularDetails() {
     when(productModel.getIsNew()).thenReturn(Boolean.FALSE);
     when(productModel.getMostPopular()).thenReturn(Boolean.TRUE);
+    when(productModel.getForRent()).thenReturn(Boolean.TRUE);
+    when(productModel.getGreatValue()).thenReturn(Boolean.TRUE);
+    when(productModel.getStaffPick()).thenReturn(Boolean.TRUE);
     populator.populate(productModel, productData);
-    Assert.assertNotEquals(productData.getProductTagValues(), BlCoreConstants.NEW);
-    Assert.assertEquals(productData.getProductTagValues(), BlCoreConstants.POPULAR);
+    assertNotEquals(productData.getProductTagValues(), BlCoreConstants.NEW);
+    assertEquals(BlCoreConstants.POPULAR,productData.getProductTagValues());
+    assertNotEquals(productData.getProductTagValues(),BlCoreConstants.GREAT_VALUE_STRING);
+    assertNotEquals(productData.getProductTagValues(),BlCoreConstants.STAFF_PICK_STRING);
   }
 
   @Test
@@ -52,9 +74,12 @@ public class BlProductTagPopulatorTest {
     when(productModel.getMostPopular()).thenReturn(Boolean.FALSE);
     when(productModel.getForRent()).thenReturn(Boolean.TRUE);
     when(productModel.getGreatValue()).thenReturn(Boolean.TRUE);
+    when(productModel.getStaffPick()).thenReturn(Boolean.TRUE);
     populator.populate(productModel, productData);
-    Assert.assertNotEquals(productData.getProductTagValues(), BlCoreConstants.NEW);
-    Assert.assertEquals(productData.getProductTagValues(), BlCoreConstants.GREAT_VALUE);
+    assertNotEquals(productData.getProductTagValues(), BlCoreConstants.NEW);
+    assertNotEquals(productData.getProductTagValues(),BlCoreConstants.POPULAR);
+    assertEquals(BlCoreConstants.GREAT_VALUE_STRING,productData.getProductTagValues());
+    assertNotEquals(productData.getProductTagValues(),BlCoreConstants.STAFF_PICK_STRING);
   }
 
   @Test
@@ -65,8 +90,10 @@ public class BlProductTagPopulatorTest {
     when(productModel.getGreatValue()).thenReturn(Boolean.FALSE);
     when(productModel.getStaffPick()).thenReturn(Boolean.TRUE);
     populator.populate(productModel, productData);
-    Assert.assertNotEquals(productData.getProductTagValues(), BlCoreConstants.NEW);
-    Assert.assertEquals(productData.getProductTagValues(), BlCoreConstants.STAFF_PICK);
+    assertNotEquals(productData.getProductTagValues(), BlCoreConstants.NEW);
+    assertNotEquals(productData.getProductTagValues(),BlCoreConstants.POPULAR);
+    assertNotEquals(productData.getProductTagValues(),BlCoreConstants.GREAT_VALUE_STRING);
+    assertEquals(BlCoreConstants.STAFF_PICK_STRING ,productData.getProductTagValues());
   }
 }
 
