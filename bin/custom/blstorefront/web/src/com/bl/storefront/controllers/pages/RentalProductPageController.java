d188711e6306f03e4a87e5aae2fae9acb6b169ad
sahanasb <sahanab@pragiti.com>
2021-04-14 21:17:55 +0530

    [added inventory conditon and date range]

--- bin/custom/blstorefront/web/src/com/bl/storefront/controllers/pages/RentalProductPageController.java
+++ bin/custom/blstorefront/web/src/com/bl/storefront/controllers/pages/RentalProductPageController.java
@@ -59,14 +59,11 @@ public class RentalProductPageController extends AbstractBlProductPageController
   			final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d");
   			date.setSelectedFromDate(startDate.format(formatter));
 				date.setSelectedToDate(endDate.format(formatter));
-  		//	date.setNumberOfDays(endDate.format(formatter));
   			model.addAttribute("datedata", date);
   		}
   		else
   		{
   			final RentalDateDto date = new RentalDateDto();
-  		//	date.setSelectedFromDate("Apr 12");
-  		//	date.setSelectedToDate("Apr 19");
   			date.setNumberOfDays("7");
   			model.addAttribute("datedata", date);
   		} // Temporary code ends here
