fb3355773e85ade41c26ee96dbd107a9e6c9f20d
ravikumar2506 <ravgupta@dminc.com>
2021-04-16 10:29:53 +0530

    Removed Unused Imports

--- bin/custom/blfacades/src/com/bl/facades/populators/BlProductPopulator.java
+++ bin/custom/blfacades/src/com/bl/facades/populators/BlProductPopulator.java
@@ -25,8 +25,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-import javax.annotation.Resource;
-
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.PredicateUtils;
 import org.apache.commons.lang3.BooleanUtils;
