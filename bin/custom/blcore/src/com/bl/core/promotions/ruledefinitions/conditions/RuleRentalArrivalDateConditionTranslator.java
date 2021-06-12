b32685db42e0e85f52b728ba5460d088a9c905e4
ritika_pragiti7 <rkohli@dminc.com>
2021-06-12 16:14:14 +0530

    [BL-643]: Committed changes for Promotion Custom Condition

---
+++ bin/custom/blcore/src/com/bl/core/promotions/ruledefinitions/conditions/RuleRentalArrivalDateConditionTranslator.java
@@ -0,0 +1,52 @@
+package com.bl.core.promotions.ruledefinitions.conditions;
+
+import de.hybris.platform.ruledefinitions.AmountOperator;
+import de.hybris.platform.ruledefinitions.conditions.builders.RuleIrAttributeConditionBuilder;
+import de.hybris.platform.ruledefinitions.conditions.builders.RuleIrGroupConditionBuilder;
+import de.hybris.platform.ruleengineservices.compiler.RuleCompilerContext;
+import de.hybris.platform.ruleengineservices.compiler.RuleConditionTranslator;
+import de.hybris.platform.ruleengineservices.compiler.RuleIrAttributeOperator;
+import de.hybris.platform.ruleengineservices.compiler.RuleIrCondition;
+import de.hybris.platform.ruleengineservices.compiler.RuleIrFalseCondition;
+import de.hybris.platform.ruleengineservices.compiler.RuleIrGroupCondition;
+import de.hybris.platform.ruleengineservices.compiler.RuleIrGroupOperator;
+import de.hybris.platform.ruleengineservices.rao.CartRAO;
+import de.hybris.platform.ruleengineservices.rule.data.RuleConditionData;
+import de.hybris.platform.ruleengineservices.rule.data.RuleConditionDefinitionData;
+import de.hybris.platform.ruleengineservices.rule.data.RuleParameterData;
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * This class is created for setting a rental arrival date
+ * in promotion conditions
+ *
+ * @author Ritika
+ */
+public class RuleRentalArrivalDateConditionTranslator implements RuleConditionTranslator {
+
+  public static final String RENTAL_ARRIVAL_DATE = "rentalArrivalDate";
+  public static final String OPERATOR = "operator";
+
+  @Override
+  public RuleIrCondition translate(final RuleCompilerContext ruleCompilerContext,final RuleConditionData condition,final RuleConditionDefinitionData ruleConditionDefinitionData) {
+
+    final Map<String, RuleParameterData> conditionParameters = condition.getParameters();
+
+    final Date rentalArrivalDate = conditionParameters.get(RENTAL_ARRIVAL_DATE).getValue();
+    final AmountOperator operatorParameter = conditionParameters.get(OPERATOR).getValue();
+
+    final RuleIrGroupCondition irRentalArrivalDateCondition = RuleIrGroupConditionBuilder
+        .newGroupConditionOf(RuleIrGroupOperator.AND).build();
+
+    if (rentalArrivalDate == null) {
+      return new RuleIrFalseCondition();
+    } else {
+      final String cartRaoVariable = ruleCompilerContext.generateVariable(CartRAO.class);
+      irRentalArrivalDateCondition.getChildren().add(RuleIrAttributeConditionBuilder.newAttributeConditionFor(cartRaoVariable)
+          .withAttribute(RENTAL_ARRIVAL_DATE).withOperator(RuleIrAttributeOperator.valueOf(operatorParameter.name())).withValue(rentalArrivalDate).build());
+    }
+    return irRentalArrivalDateCondition;
+  }
+}
+
