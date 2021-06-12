2c85239678598df1fda1fd018319ac084b471698
sahanasb <sahanab@pragiti.com>
2021-06-12 22:09:07 +0530

    [reverting it to fst one]

--- bin/custom/blstorefront/web/webroot/_ui/responsive/common/js/blcustom.js
+++ bin/custom/blstorefront/web/webroot/_ui/responsive/common/js/blcustom.js
@@ -242,3 +242,5 @@ if($(".arrival").hasClass("nextAvailDate") && !$("#addToCartButton").hasClass("j
     $(" #productDates .input-group").addClass("red-border");
 }
 $(".js-hr-tag").last().hide();
+
+777777777777777777777777777
