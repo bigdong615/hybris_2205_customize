651a4568ab335170f776b065f043453340883e4d
sahanasb <sahanab@pragiti.com>
2021-06-29 18:54:55 +0530

    [after conflict adding my chnages]

--- bin/custom/blstorefront/web/src/com/bl/storefront/controllers/misc/AddToCartController.java
+++ bin/custom/blstorefront/web/src/com/bl/storefront/controllers/misc/AddToCartController.java
@@ -147,7 +147,7 @@ public class AddToCartController extends AbstractController {
 
 		model.addAttribute("product", productFacade.getProductForCodeAndOptions(code, Arrays.asList(ProductOption.BASIC)));
 		final List<ProductOption> PRODUCT_OPTIONS = Arrays.asList(ProductOption.BASIC, ProductOption.PRICE,
-				ProductOption.REQUIRED_DATA, ProductOption.GALLERY, ProductOption.STOCK);
+				ProductOption.REQUIRED_DATA, ProductOption.GALLERY, ProductOption.STOCK,ProductOption.REQUIRED_WISHLIST);
 		final Integer productsLimit = Integer.valueOf(Config.getInt(PRODUCT_LIMIT, 50));
 		final List<ProductReferenceData> productReferences = productFacade.getProductReferencesForCode(code,
 				getEnumerationService().getEnumerationValues(ProductReferenceTypeEnum._TYPECODE), PRODUCT_OPTIONS, productsLimit);
