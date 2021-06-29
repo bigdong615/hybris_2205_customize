380ceec248d23bd675f9de05c08cc9659cfb6502
sahanasb <sahanab@pragiti.com>
2021-06-29 15:16:53 +0530

    [made separate js file for wishlist ]

--- bin/custom/blstorefront/web/webroot/_ui/responsive/common/js/acc.wishlist.js
+++ bin/custom/blstorefront/web/webroot/_ui/responsive/common/js/acc.wishlist.js
@@ -1,120 +1,57 @@
-ACC.account = {
-
-	_autoload: [
-		"accountLink"
-	],
-
-	accountLink: function () {
-	        var bookmarkicons = document.querySelectorAll(".bookmarkicons");
-           console.log(bookmarkicons.length);
-              for(var i=0;i<bookmarkicons.length;i++){
-                     bookmarkicons[i].id ="bookmarkicons-"+(i+1);
-                     alert("for loop is running");
-                      }
+ var bookmarkicons = document.querySelectorAll(".bookmarkicons");
+               console.log(bookmarkicons.length);
+                  for(var i=0;i<bookmarkicons.length;i++){
+                         bookmarkicons[i].id ="bookmarkicons-"+(i+1);
+                          }
 
 
-          $(document).on('click',".js-add-to-wishlist", function(e) {
-          alert("inside wishlist");
-                                  e.preventDefault();
-  //                                  var productCode = $(this).attr('data-product-code');
-  //                                  var targetUrl = $(".add_to_wishList_form").attr("action");
+              $(document).on('click',".js-add-to-wishlist", function(e) {
+                                      e.preventDefault();
+                                       var bookmarkId =this.getAttribute("id");
+                                       console.log(bookmarkId);
+                                        var productCode = $(this).attr('data-product-code');
+                                        var targetAddUrl    = ACC.config.encodedContextPath + "/wishlist/add"
+                                        var targetRemoveUrl = ACC.config.encodedContextPath + "/removewishlist"
+                                        var bookmarkValue = $(this).attr('data-bookmark-value');
+                                        if((document.getElementById(bookmarkId).classList.contains("set")))
+                                        {
+                                        $.ajax({
+                                                       url: targetRemoveUrl,
+                                                       type: 'POST',
+                                                       data: {removeproductCode: productCode},
+                                                       success: function (response) {
+                                                       if(response === 'Success'){
+                                                       alert("produt removed");
+                                                        document.getElementById(bookmarkId).classList.remove("set");
+                                                       }
+                                                       else{
+                                                       console.log("found error while removing the product");
+                                                       }
 
-                                   var bookmarkId =this.getAttribute("id");
-                                     console.log(bookmarkId);
-                                     console.log(document.getElementById(bookmarkId));
-                                     console.log(ACC.config.encodedContextPath + "/removewishlist");
-                                   if(document.getElementById(bookmarkId).classList.contains("bookmark-checked")){
-                                    var productCode = $(this).attr('data-product-code');
-                                   $.ajax({
-                                      url: ACC.config.encodedContextPath + "/removewishlist",
-                                      type: 'POST',
-                                      data: {removeproductCode: productCode},
-                                      success: function (response) {
-                                      if(response == success){
-                                      alert("product removed from wishlist");
-                                      //shd make button red here
-                                      }
-                                      else{
-                                      //shd not make button red here
-                                      }
+                                                         },
+                                                       error: function (jqXHR, textStatus, errorThrown) {
+                                                           console.log("The following error occurred: " +jqXHR, textStatus, errorThrown);
+                                                        }
+                                                 });
 
-                                        },
-                                      error: function (jqXHR, textStatus, errorThrown) {
-                                          console.log("The following error occurred: " +jqXHR, textStatus, errorThrown);
-                                       }
-                                   });
-                                   document.getElementById(bookmarkId).classList.remove("bookmark-checked");
-                                   }
-                                   else{
-                                    var productCode = $(this).attr('data-product-code');
-                                    var targetUrl = $(".add_to_wishList_form").attr("action");
-                                    $.ajax({
-                                            url: targetUrl,
-                                            type: 'POST',
-                                            data: {productwishlistCode: productCode},
-                                             success: function (response) {
-                                                    alert("product added to wishlist");
-                                                   },
-                                             error: function (jqXHR, textStatus, errorThrown) {
-                                                 // log the error to the console
-                                             console.log("The following error occurred: " +jqXHR, textStatus, errorThrown);
-                                             }
-                                     });
-                                   document.getElementById(bookmarkId).classList.add("bookmark-checked");
-                                   }
-  //
-  //var productCode = $(this).attr('data-product-code');
-  //                                 $.ajax({
-  //                                            url: ACC.config.encodedContextPath + "/removewishlist",
-  //                                            type: 'POST',
-  //                                            data: {removeproductCode: productCode},
-  //                                            success: function (response) {
-  //                                            alert("product removed to wishlist");
-  //                                              //addToCartToAdded();
-  //                                            },
-  //                                            error: function (jqXHR, textStatus, errorThrown) {
-  //                                                  // log the error to the console
-  //                                                  console.log("The following error occurred: " +jqXHR, textStatus, errorThrown);
-  //                                            }
-  //                                 });
-
-            });
-
-
-
-  // $(document).on('click',".js-remove-from-wishlist", function(e) {
-  //        alert("inside wishlist");
-  //                                e.preventDefault();
-  //                                 var productCode = $(this).attr('data-product-code');
-  //                                 var targetUrl = $(".remove_from_wishList_").attr("action");
-  //                                 $.ajax({
-  //                                            url: targetUrl,
-  //                                            type: 'POST',
-  //                                            data: {removeproductCode: productCode},
-  //                                            success: function (response) {
-  //                                            alert("product added to cart");
-  //                                              //addToCartToAdded();
-  //                                            },
-  //                                            error: function (jqXHR, textStatus, errorThrown) {
-  //                                                  // log the error to the console
-  //                                                  console.log("The following error occurred: " +jqXHR, textStatus, errorThrown);
-  //                                            }
-  //                                 });
-  //
-  //          });
-
-
-  	}
-  	};
-
-
-
-		/** Added for BL-31 to make validation for registration **/
-
-
-
-
-
-
-	}
-};
+                                        }
+                                        else{
+                                       $.ajax({
+                                          url: targetAddUrl,
+                                          type: 'POST',
+                                          data: {productwishlistCode: productCode},
+                                          success: function (response) {
+                                          if(response === 'Success'){
+                                          alert("produt added");
+                                           document.getElementById(bookmarkId).classList.add("set");
+                                          }
+                                          else{
+                                          console.log("found error while adding product to wishlist");
+                                          }
+                                            },
+                                          error: function (jqXHR, textStatus, errorThrown) {
+                                              console.log("The following error occurred: " +jqXHR, textStatus, errorThrown);
+                                           }
+                                       });
+                                     }
+                });
