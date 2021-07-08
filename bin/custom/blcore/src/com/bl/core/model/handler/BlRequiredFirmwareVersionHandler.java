14dd2adeb6233c42ad23d8d2e4dd7a1bb5f7047b
ravikumar2506 <ravgupta@dminc.com>
2021-07-08 13:44:09 +0530

    Updated changes as per the PR review comments

--- bin/custom/blcore/src/com/bl/core/model/handler/BlRequiredFirmwareVersionHandler.java
+++ bin/custom/blcore/src/com/bl/core/model/handler/BlRequiredFirmwareVersionHandler.java
@@ -1,5 +1,6 @@
 package com.bl.core.model.handler;
 
+import com.bl.core.model.BlProductModel;
 import com.bl.core.model.BlSerialProductModel;
 import com.bl.logging.BlLogger;
 import de.hybris.platform.servicelayer.model.attribute.DynamicAttributeHandler;
@@ -19,16 +20,36 @@ public class BlRequiredFirmwareVersionHandler implements
   private static final Logger LOG = Logger.getLogger(BlRequiredFirmwareVersionHandler.class);
 
   @Override
-  public String get(BlSerialProductModel serialProduct) {
-    if (Objects.nonNull(serialProduct) && Objects.nonNull(serialProduct.getBlProduct())) {
-      return serialProduct.getBlProduct().getFirmwareVersion();
+  public String get(final BlSerialProductModel serialProduct) {
+    if (Objects.nonNull(serialProduct)) {
+   	 if(Objects.nonNull(serialProduct.getBlProduct())) {
+   		 return getFirmwareVersionOfSku(serialProduct.getBlProduct());
+   	 }   	 
+   	 BlLogger.logFormatMessageInfo(LOG, Level.ERROR, "No SKU is assigned to Serial Product with ProductId : {}", 
+   			 serialProduct.getProductId());
+   	 return StringUtils.EMPTY;
     }
-    BlLogger.logMessage(LOG, Level.ERROR, "Enable to get firmware version from SKU");
+    BlLogger.logMessage(LOG, Level.ERROR, "Cannot evaluate the value for BlSerialProduct.skuFirmwareVersion because Serial is null");
     return StringUtils.EMPTY;
+  }
+  
+  /**
+   * Gets the firmware version of sku.
+   *
+   * @param sku the sku
+   * @return the firmware version of sku
+   */
+  private String getFirmwareVersionOfSku(final BlProductModel sku) {
+	  final String skuFirmwareVersion = sku.getFirmwareVersion();
+	  if(StringUtils.isNotBlank(skuFirmwareVersion)) {
+		  return skuFirmwareVersion;
+	  }
+	  BlLogger.logFormatMessageInfo(LOG, Level.ERROR, "No Firmware version found on SKU with code : {}", sku.getCode());
+	  return StringUtils.EMPTY;
   }
 
   @Override
-  public void set(BlSerialProductModel serialProduct, String stringValue) {
+  public void set(final BlSerialProductModel serialProduct, final String stringValue) {
     BlLogger.logMessage(LOG, Level.ERROR,
         "Setter for attribute BlSerialProduct.skuFirmwareVersion is not supported");
     throw new UnsupportedOperationException();
