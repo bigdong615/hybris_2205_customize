28bff6312a5d748a08ce5b18564387f73ec1ed3b
Neeraj Singh <neerajs@pragiti.com>
2021-07-13 04:27:24 +0530

    [BL-623] removed unused import

--- bin/custom/blfacades/src/com/bl/facades/cart/impl/DefaultBlCartFacade.java
+++ bin/custom/blfacades/src/com/bl/facades/cart/impl/DefaultBlCartFacade.java
@@ -30,7 +30,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
