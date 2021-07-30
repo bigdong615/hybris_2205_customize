e65b61f08e6eb263701583e31170d81b562776b8
LoharNamrata <namratalohar@pragiti.com>
2021-07-30 17:14:49 +0530

    code commit for -
    1. Getting server error when selecting any shiiping method after session timeout - BL-685
    2. Intermittent issue with the pick up section - BL_935

--- bin/custom/blstorefront/web/webroot/_ui/responsive/common/js/blcustomshipping.js
+++ bin/custom/blstorefront/web/webroot/_ui/responsive/common/js/blcustomshipping.js
@@ -145,7 +145,10 @@ function removeClass(){
              $('.page-loader-new-layout').hide();
          },
          error: function (data) {
-             $('.page-loader-new-layout').hide();
+            if(data != null && data.statusText == 'parsererror') {
+                window.location.reload();
+            }
+            $('.page-loader-new-layout').hide();
          }
      });
   }
@@ -237,6 +240,9 @@ function removeClass(){
             $('.page-loader-new-layout').hide();
         },
         error: function (data) {
+            if(data != null && data.statusText == 'parsererror') {
+                window.location.reload();
+            }
             $('.page-loader-new-layout').hide();
         }
     });
@@ -336,6 +342,9 @@ function removeClass(){
                  $('.page-loader-new-layout').hide();
              },
              error: function (data) {
+                if(data != null && data.statusText == 'parsererror') {
+                    window.location.reload();
+                }
                  $('.page-loader-new-layout').hide();
              }
          });
@@ -532,7 +541,7 @@ function removeClass(){
             $('.page-loader-new-layout').show();
         },
         success: function (data) {
-            if(data != null && data.length != 0) {
+            if(data != null && data.length != 0 && !data.startsWith('<!DOCTYPE html>')) {
                 let partnerDelivery = '';
                 for (let i = 0; i < data.length; i++) {
                     if(i == 0 && data.length == 1) {
@@ -590,7 +599,10 @@ function removeClass(){
                 showErrorNotificationPickUp('Rental Dates not eligible for the selected shipping option!!');
             }
         },
-        complete: function() {
+        complete: function(data) {
+            if(data.startsWith('<!DOCTYPE html>')) {
+                window.location.reload();
+            }
             $('.page-loader-new-layout').hide();
         },
         error: function (error) {
@@ -743,7 +755,7 @@ function removeClass(){
                             $('.page-loader-new-layout').show();
                        },
                        success: function (data) {
-                           if(data != null && data.length != 0) {
+                           if(data != null && data.length != 0 && !data.startsWith('<!DOCTYPE html>')) {
                                let sameDayShippingModes = '<b>Delivery Window</b>';
                                sameDayShippingModes += '<select id="same-day-shipping-methods-select-box" class="selectpicker mt-2"' +
                                                         ' onChange="onChangeOfSameDayShippingMethodForCost()">';
@@ -785,9 +797,15 @@ function removeClass(){
                            }
                        },
                        complete: function() {
+                            if(data.startsWith('<!DOCTYPE html>')) {
+                                window.location.reload();
+                            }
                            $('.page-loader-new-layout').hide();
                        },
                        error: function (data) {
+                            if(data != null && data.statusText == 'parsererror') {
+                                window.location.reload();
+                            }
                            $('.page-loader-new-layout').hide();
                        }
                    });
@@ -796,10 +814,16 @@ function removeClass(){
                 }
             },
             complete: function() {
+                if(data != null && data.statusText == 'parsererror') {
+                    window.location.reload();
+                }
                 $('.page-loader-new-layout').hide();
             },
             error: function (data) {
-                   $('.page-loader-new-layout').hide();
+                if(data != null && data.statusText == 'parsererror') {
+                    window.location.reload();
+                }
+                $('.page-loader-new-layout').hide();
             }
         });
     } else {
