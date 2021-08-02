f49adb9d27c2eb5bd83a5ba1a4486c3a5dee6606
KrishanVashishth <kkvashishth@dminc.com>
2021-08-02 12:40:35 +0530

    BL-986 : Committed the controller changes

--- bin/custom/blbackoffice/backoffice/src/com/bl/backoffice/widget/controller/order/refund/BlOrderRefundController.java
+++ bin/custom/blbackoffice/backoffice/src/com/bl/backoffice/widget/controller/order/refund/BlOrderRefundController.java
@@ -24,6 +24,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.zkoss.util.Locales;
+import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.Doublebox;
 import org.zkoss.zul.Messagebox;
@@ -42,6 +43,18 @@ public class BlOrderRefundController extends DefaultWidgetController {
   private static final String OUT_CONFIRM = "confirmRefund";
   private static final String COMPLETE = "completed";
   private static final String IN_SOCKET = "inputObject";
+  private static final String REFUND_STATUS_TITLE = "customersupportbackoffice.refundorder.refund.status";
+  private static final String REFUND_ERROR_MSG = "customersupportbackoffice.refundorder.error.mesg";
+  private static final String REFUND_TITLE = "customersupportbackoffice.refundorder.confirm.title";
+  private static final String REFUND_BUTTON = "refundrequest";
+  private static final String REFUND_INPUT_ERROR = "customersupportbackoffice.refundorder.input.error";
+  private static final String REFUND_COMPLETE_MSG = "customersupportbackoffice.refundorder.successfull.mesg";
+  private static final String ORDER_PAYMENT_NOT_CAPTURED = "customersupportbackoffice.refundorder.payment.not.captured";
+  private static final String INVALID_ORDER_TYPE = "customersupportbackoffice.refundorder.invalid.order.type";
+  private static final String EMPTY_AMOUNT = "customersupportbackoffice.refundorder.empty.amount";
+  private static final String NO_PAYMENT_TRANSACTION = "customersupportbackoffice.refundorder.empty.transactionId";
+  private static final String INVALID_ORDER_AMOUNT = "customersupportbackoffice.refundorder.higheramount";
+  private static final String CANCEL_BUTTON = "cancelChanges";
 
   @Resource
   private BrainTreePaymentService brainTreePaymentService;
@@ -70,7 +83,7 @@ public class BlOrderRefundController extends DefaultWidgetController {
   public void initPartialRefundForm(final OrderModel inputOrder) {
     this.setOrderModel(inputOrder);
     this.getWidgetInstanceManager().setTitle(new StringBuilder(this.getWidgetInstanceManager()
-        .getLabel("customersupportbackoffice.refundorder.confirm.title")).append(
+        .getLabel(REFUND_TITLE)).append(
         this.getOrderModel().getCode()).toString());
     this.transactionId.setValue(getPaymentTxnId());
     this.setAmountInTextBox();
@@ -100,11 +113,11 @@ public class BlOrderRefundController extends DefaultWidgetController {
     return decimalFormat.format(amount);
   }
 
-  @ViewEvent(componentID = "refundrequest", eventName = "onClick")
+  @ViewEvent(componentID = REFUND_BUTTON, eventName = Events.ON_CLICK)
   public void refundOrderAmount() {
     final String errorMessage = this.getValidateOrderMessage();
     if (StringUtils.isNotEmpty(errorMessage)) {
-      showMessageBox(errorMessage, this.getLabel("customersupportbackoffice.refundorder.input.error"));
+      showMessageBox(errorMessage, this.getLabel(REFUND_INPUT_ERROR));
       return;
     }
     final double refundAmount = this.amount.getValue();
@@ -118,21 +131,20 @@ public class BlOrderRefundController extends DefaultWidgetController {
           brainTreePaymentService.refundTransaction(request);
       if (result.isSuccess()) {
         BlLogger.logMessage(LOG, Level.DEBUG, "Refund Txn has been initiated successfully.");
-        showMessageBox(this.getLabel("customersupportbackoffice.refundorder.successfull.mesg"));
+        showMessageBox(this.getLabel(REFUND_COMPLETE_MSG));
         return;
       }
       final String refundErrorMessage = new StringBuilder(result.getErrorCode()).append(
           BlCoreConstants.HYPHEN).append(result.getErrorMessage()).toString();
       BlLogger.logMessage(LOG, Level.ERROR, refundErrorMessage);
-      this.showMessageBox(refundErrorMessage);
-      notificationService.notifyUser(StringUtils.EMPTY, BlloggingConstants.MSG_CONST,
-          NotificationEvent.Level.FAILURE, refundErrorMessage);
+      this.showMessageBox(refundErrorMessage, this.getLabel(REFUND_STATUS_TITLE));
     } catch (final AdapterException e) {
       BlLogger.logMessage(LOG, Level.ERROR,
-          this.getLabel("customersupportbackoffice.refundorder.error.mesg"));
+          this.getLabel(REFUND_ERROR_MSG));
+      this.showMessageBox(e.getMessage(), this.getLabel(REFUND_STATUS_TITLE));
       notificationService.notifyUser(StringUtils.EMPTY, BlloggingConstants.MSG_CONST,
           NotificationEvent.Level.FAILURE,
-          this.getLabel("customersupportbackoffice.refundorder.error.mesg"));
+          this.getLabel(REFUND_ERROR_MSG));
     }
   }
 
@@ -145,18 +157,16 @@ public class BlOrderRefundController extends DefaultWidgetController {
     final OrderModel order = this.getOrderModel();
     String message = StringUtils.EMPTY;
     if (OrderStatus.COMPLETED.equals(order.getStatus())) {
-      message = this.getLabel("customersupportbackoffice.refundorder.invalid.order.type");
+      message = this.getLabel(INVALID_ORDER_TYPE);
     } else if (CollectionUtils.isEmpty(order.getGiftCard())
         && BooleanUtils.isFalse(order.getIsCaptured())) {
-      message = this.getLabel("customersupportbackoffice.refundorder.payment.not.captured");
+      message = this.getLabel(ORDER_PAYMENT_NOT_CAPTURED);
     } else if (this.amount.getValue() == null) {
-      message = this.getLabel("customersupportbackoffice.refundorder.empty.amount");
+      message = this.getLabel(EMPTY_AMOUNT);
     } else if (StringUtils.isEmpty(this.transactionId.getValue())) {
-      message = this.getLabel("customersupportbackoffice.refundorder.empty.transactionId");
-    } else if (this.amount.getValue() <= 0) {
-      message = this.getLabel("customersupportbackoffice.refundorder.lessthanzero.amount");
+      message = this.getLabel(NO_PAYMENT_TRANSACTION);
     } else if (this.amount.getValue() > order.getTotalPrice()) {
-      message = this.getLabel("customersupportbackoffice.refundorder.higheramount");
+      message = this.getLabel(INVALID_ORDER_AMOUNT);
     }
     return message;
   }
@@ -190,11 +200,10 @@ public class BlOrderRefundController extends DefaultWidgetController {
    */
   protected void showMessageBox(final String message, final String errorMessage) {
     if (StringUtils.isNotEmpty(errorMessage)) {
-      Messagebox.show(message,
-          this.getLabel("customersupportbackoffice.refundorder.input.error"),
-          Messagebox.OK, Messagebox.ERROR);
+      Messagebox.show(message, this.getLabel(REFUND_INPUT_ERROR), Messagebox.OK, Messagebox.ERROR);
     } else {
-      Messagebox.show(message);
+      Messagebox.show(message, this.getLabel(REFUND_STATUS_TITLE), Messagebox.OK,
+          Messagebox.INFORMATION);
     }
     this.sendOutput(OUT_CONFIRM, COMPLETE);
   }
@@ -202,7 +211,7 @@ public class BlOrderRefundController extends DefaultWidgetController {
   /**
    * Method will be invoked when user will click on CANCEL button
    */
-  @ViewEvent(componentID = "cancelChanges", eventName = "onClick")
+  @ViewEvent(componentID = CANCEL_BUTTON, eventName = Events.ON_CLICK)
   public void close() {
     this.sendOutput(OUT_CONFIRM, COMPLETE);
   }
@@ -217,9 +226,5 @@ public class BlOrderRefundController extends DefaultWidgetController {
 
   public NotificationService getNotificationService() {
     return notificationService;
-  }
-
-  public void setNotificationService(final NotificationService notificationService) {
-    this.notificationService = notificationService;
   }
 }
