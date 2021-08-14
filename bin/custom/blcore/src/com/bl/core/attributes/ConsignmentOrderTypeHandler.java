cec59165a73735f6f0749b1c6d61465bde906f77
Sunil Sahu <sksahu@dminc.com>
2021-08-14 17:13:12 +0530

    Fixed for CS team setting vip order type

--- bin/custom/blcore/src/com/bl/core/attributes/ConsignmentOrderTypeHandler.java
+++ bin/custom/blcore/src/com/bl/core/attributes/ConsignmentOrderTypeHandler.java
@@ -21,10 +21,10 @@ public class ConsignmentOrderTypeHandler extends
   @Override
   public OrderTypeEnum get(final ConsignmentModel consignmentModel) {
 
-    if (null == consignmentModel || null == consignmentModel.getOrder() || null == consignmentModel
-        .getOrder().getOrderType()) {
+    if (null != consignmentModel && (null == consignmentModel.getOrder() || null == consignmentModel
+        .getOrder().getOrderType())) {
 
-      BlLogger.logFormatMessageInfo(LOGGER, Level.ERROR,
+      BlLogger.logFormatMessageInfo(LOGGER, Level.DEBUG,
           "Returning null value for order type of consignment as orderModel or order type is found to be null");
 
       return null;
