afeef363c25fe35267ae494799de62d0ff06b692
Moumita Patra <mpatra@dminc.com>
2021-08-21 21:54:10 -0400

    feature/BL-901 - unused import removed

--- bin/custom/bltaxapi/src/com/bl/tax/service/impl/BlAvalaraTaxCalculationService.java
+++ bin/custom/bltaxapi/src/com/bl/tax/service/impl/BlAvalaraTaxCalculationService.java
@@ -1,13 +1,7 @@
 package com.bl.tax.service.impl;
 
-import com.bl.core.enums.ItemBillingChargeTypeEnum;
 import com.bl.tax.TaxResponse;
-import com.bl.tax.constants.BltaxapiConstants;
 import de.hybris.platform.core.model.order.AbstractOrderModel;
-import de.hybris.platform.servicelayer.model.ModelService;
-import java.math.BigDecimal;
-import org.apache.commons.lang3.BooleanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * This class is created for setting avalara tax with order
