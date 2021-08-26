e5a481d5d1cf3398fbfe8ecf87c67c89ef41f450
vijayvv7060 <vvishwakarma@dminc.com>
2021-08-26 15:30:31 +0530

    [BL-963]: changes related to remove empty saved cart entry from saved cart and update quantity on cart page for new gear product.

--- bin/custom/blfacades/src/com/bl/facades/cart/impl/DefaultBlSaveCartFacade.java
+++ bin/custom/blfacades/src/com/bl/facades/cart/impl/DefaultBlSaveCartFacade.java
@@ -5,12 +5,14 @@ import com.bl.facades.cart.BlSaveCartFacade;
 import com.bl.facades.constants.BlFacadesConstants;
 import de.hybris.platform.commercefacades.order.data.CartData;
 import de.hybris.platform.commercefacades.order.impl.DefaultSaveCartFacade;
+import de.hybris.platform.commerceservices.order.CommerceSaveCartException;
 import de.hybris.platform.commerceservices.search.pagedata.PageableData;
 import de.hybris.platform.commerceservices.search.pagedata.SearchPageData;
 import de.hybris.platform.converters.Converters;
 import de.hybris.platform.core.enums.OrderStatus;
 import de.hybris.platform.core.model.order.CartModel;
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.Resource;
 import org.apache.commons.collections4.CollectionUtils;
@@ -41,20 +43,39 @@ public class DefaultBlSaveCartFacade extends DefaultSaveCartFacade implements Bl
         getBaseSiteService().getCurrentBaseSite(), getUserService().getCurrentUser(), orderStatus);
     // remove discontinue entry from cart.
     StringBuilder removedBufferEntries = new StringBuilder();
+    List<String> emptyCartCodeList = new ArrayList<>();
     if(CollectionUtils.isNotEmpty(savedCartModels.getResults())) {
       savedCartModels.getResults().forEach(cartModel -> {
         String removedEntry =  blCartFacade.removeDiscontinueProductFromCart(cartModel, Boolean.FALSE);
             if(StringUtils.isNotEmpty(removedEntry)) {
               removedBufferEntries.append(BlFacadesConstants.COMMA_SEPERATER).append(removedEntry);
             }
+            /** Collecting all cart code which don't have entry*/
+        if (CollectionUtils.isEmpty(cartModel.getEntries())) {
+          emptyCartCodeList.add(cartModel.getCode());
+        }
       });
     }
+    SearchPageData<CartModel> updatedSavedCartModels = savedCartModels;
+    /** Removing all cart which don't have entry*/
+    if(CollectionUtils.isNotEmpty(emptyCartCodeList)){
+      emptyCartCodeList.forEach(cartCode ->{
+        try {
+          flagForDeletion(cartCode);
+        } catch (CommerceSaveCartException e) {
+          e.printStackTrace();
+        }
+      });
+      /** pick updated saved cart data which show on saved cart page.*/
+      updatedSavedCartModels = getCommerceSaveCartService().getSavedCartsForSiteAndUser(pageableData,
+          getBaseSiteService().getCurrentBaseSite(), getUserService().getCurrentUser(), orderStatus);
+    }
     model.addAttribute(BlFacadesConstants.REMOVE_ENTRIES,removedBufferEntries.toString());
-    result.setPagination(savedCartModels.getPagination());
-    result.setSorts(savedCartModels.getSorts());
+    result.setPagination(updatedSavedCartModels.getPagination());
+    result.setSorts(updatedSavedCartModels.getSorts());
 
     final List<CartData> savedCartDatas = Converters
-        .convertAll(savedCartModels.getResults(), getCartConverter());
+        .convertAll(updatedSavedCartModels.getResults(), getCartConverter());
 
     result.setResults(savedCartDatas);
     return result;
