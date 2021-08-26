fc956ad48eef5ce5962074c9409d2c8ffe4a3895
PLB-3080\admin <aditi@pragiti.com>
2021-08-26 18:48:46 +0530

    [BL-982] : "Committed code for removal of order entry"

--- bin/custom/blbackoffice/backoffice/src/com/bl/backoffice/wizards/handler/BlDefaultEditorAreaLogicHandler.java
+++ bin/custom/blbackoffice/backoffice/src/com/bl/backoffice/wizards/handler/BlDefaultEditorAreaLogicHandler.java
@@ -1,14 +1,34 @@
 package com.bl.backoffice.wizards.handler;
 
+import de.hybris.platform.core.model.order.AbstractOrderEntryModel;
+import de.hybris.platform.core.model.order.AbstractOrderModel;
+import de.hybris.platform.core.model.order.OrderModel;
+import de.hybris.platform.order.exceptions.CalculationException;
+import de.hybris.platform.ordersplitting.model.ConsignmentEntryModel;
+import de.hybris.platform.ordersplitting.model.ConsignmentModel;
+import de.hybris.platform.ordersplitting.model.StockLevelModel;
+import de.hybris.platform.servicelayer.model.ModelService;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import com.bl.core.model.BlProductModel;
+import com.bl.core.model.BlSerialProductModel;
 import com.bl.core.order.impl.DefaultBlCalculationService;
+import com.bl.core.stock.BlStockLevelDao;
 import com.bl.logging.BlLogger;
+import com.google.common.collect.Lists;
 import com.hybris.cockpitng.dataaccess.facades.object.exceptions.ObjectSavingException;
 import com.hybris.cockpitng.engine.WidgetInstanceManager;
 import com.hybris.cockpitng.widgets.baseeditorarea.DefaultEditorAreaLogicHandler;
-import de.hybris.platform.core.model.order.OrderModel;
-import de.hybris.platform.order.exceptions.CalculationException;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
 
 /**
  * This class created to call avalara while saving the order from cscockpit
@@ -19,6 +39,12 @@ public class BlDefaultEditorAreaLogicHandler extends DefaultEditorAreaLogicHandl
   private static final Logger LOG = Logger.getLogger(BlDefaultEditorAreaLogicHandler.class);
 
   private DefaultBlCalculationService defaultBlCalculationService;
+  
+	@Resource(name = "modelService")
+	ModelService modelService;
+
+	@Resource(name = "blStockLevelDao")
+	private BlStockLevelDao blStockLevelDao;
 
   /**
    * This method call when order is saving
@@ -28,6 +54,14 @@ public class BlDefaultEditorAreaLogicHandler extends DefaultEditorAreaLogicHandl
     if (currentObject instanceof OrderModel) {
       OrderModel orderModel = (OrderModel) currentObject;
         orderModel.setCalculated(false);
+     	final List<Object> previousChangedOrderEntrysList = getPreviousChangedOrderEntrysList(orderModel);
+
+		if (CollectionUtils.isNotEmpty(previousChangedOrderEntrysList))
+		{
+			final List<AbstractOrderEntryModel> updatedOrderEntry = orderModel.getEntries();
+			previousChangedOrderEntrysList.removeIf(updatedOrderEntry::contains);
+		}
+		removeEntryFromConsignment(orderModel, previousChangedOrderEntrysList);
         orderModel.getEntries().forEach(abstractOrderEntryModel -> abstractOrderEntryModel.setCalculated(Boolean.FALSE));
       try {
         getDefaultBlCalculationService().recalculateOrderForTax(orderModel);
@@ -38,7 +72,123 @@ public class BlDefaultEditorAreaLogicHandler extends DefaultEditorAreaLogicHandl
      }
     return super.performSave(widgetInstanceManager , currentObject);
   }
+  
+  /**
+	 * method is used to remove entry from consignment
+	 *
+	 * @param orderModel
+	 * @param previousChangedOrderEntrysList
+	 */
+	private void removeEntryFromConsignment(final OrderModel orderModel, final List<Object> previousChangedOrderEntrysList)
+	{
+		if (CollectionUtils.isNotEmpty(previousChangedOrderEntrysList))
+		{
+			final AbstractOrderEntryModel previousChangedOrderEntry = (AbstractOrderEntryModel) previousChangedOrderEntrysList
+					.iterator().next();
+			final String orderEntrySkuPk = previousChangedOrderEntry.getProduct().getPk().toString();
+			final List<ConsignmentEntryModel> consignmentEntryToRemove = new ArrayList<>();
+			final List<ConsignmentModel> consignmentToRemove = new ArrayList<>();
+			for (final ConsignmentModel consignment : orderModel.getConsignments())
+			{
+				removeConsignmentEntry(orderEntrySkuPk, consignmentEntryToRemove, consignment);
+			}
+			modelService.removeAll(consignmentEntryToRemove);
+			removeConsignment(orderModel, consignmentToRemove);
+			modelService.removeAll(consignmentToRemove);
+		}
+	}
 
+	/**
+	 * method is used remove consignment entry if no serial is available in it
+	 *
+	 * @param orderEntrySkuPk
+	 * @param consignmentEntryToRemove
+	 * @param consignment
+	 */
+	private void removeConsignmentEntry(final String orderEntrySkuPk, final List<ConsignmentEntryModel> consignmentEntryToRemove,
+			final ConsignmentModel consignment)
+	{
+		for (final ConsignmentEntryModel consignmentEntry : consignment.getConsignmentEntries())
+		{
+			final List<BlProductModel> updatedSerialList = new ArrayList<>();
+			consignmentEntry.getSerialProducts().forEach(serial -> {
+				if (serial instanceof BlSerialProductModel
+						&& !orderEntrySkuPk.equals(((BlSerialProductModel) serial).getBlProduct().getPk().toString())) // NOSONAR
+				{
+					updatedSerialList.add(serial);
+				}
+				else
+				{
+					updateStockForSerial(consignment, serial);
+				}
+			});
+			if (CollectionUtils.isEmpty(updatedSerialList))
+			{
+				consignmentEntryToRemove.add(consignmentEntry);
+			}
+			else
+			{
+				consignmentEntry.setSerialProducts(updatedSerialList);
+				modelService.save(consignmentEntry);
+				modelService.refresh(consignmentEntry);
+			}
+		}
+	}
+
+	/**
+	 * method is used to update stock from removed entry
+	 * @param consignment
+	 * @param serial
+	 */
+	private void updateStockForSerial(final ConsignmentModel consignment, final BlProductModel serial)
+	{
+		if (serial instanceof BlSerialProductModel)
+		{
+			final Collection<StockLevelModel> findSerialStockLevelForDate = blStockLevelDao.findSerialStockLevelForDate(
+					serial.getCode(), consignment.getOptimizedShippingStartDate(), consignment.getOptimizedShippingEndDate());
+			if (CollectionUtils.isNotEmpty(findSerialStockLevelForDate))
+			{
+				findSerialStockLevelForDate.forEach(stockLevel -> {
+					stockLevel.setHardAssigned(false);
+					stockLevel.setReservedStatus(false);
+					((BlSerialProductModel) serial).setHardAssigned(false); // NOSONAR
+				});
+			}
+		}
+	}
+
+	/**
+	 * method is used to remove consignment if all the consignment entries has been removed
+	 *
+	 * @param orderModel
+	 * @param consignmentToRemove
+	 */
+	private void removeConsignment(final OrderModel orderModel, final List<ConsignmentModel> consignmentToRemove)
+	{
+		for (final ConsignmentModel consignment : orderModel.getConsignments())
+		{
+			modelService.refresh(consignment);
+			if (CollectionUtils.isEmpty(consignment.getConsignmentEntries()))
+			{
+				consignmentToRemove.add(consignment);
+			}
+		}
+	}
+
+	/**
+	 * method is used to get the original value for order entry
+	 * @param orderModel
+	 * @return
+	 */
+	private List<Object> getPreviousChangedOrderEntrysList(final AbstractOrderModel orderModel)
+	{
+		final Object previousValue = orderModel.getItemModelContext().getOriginalValue("entries");
+		if (previousValue instanceof List)
+		{
+			return Lists.newArrayList((List) previousValue);
+		}
+		return Collections.emptyList();
+	}
 
   public DefaultBlCalculationService getDefaultBlCalculationService() {
     return defaultBlCalculationService;
