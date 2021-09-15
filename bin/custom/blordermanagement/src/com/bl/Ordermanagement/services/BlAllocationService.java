877835de9f43bebf2f5b1e73c9c8ac8918272a50
Moumita Patra <mpatra@dminc.com>
2021-09-15 18:56:38 -0400

    Changes for reshuffler job

--- bin/custom/blordermanagement/src/com/bl/Ordermanagement/services/BlAllocationService.java
+++ bin/custom/blordermanagement/src/com/bl/Ordermanagement/services/BlAllocationService.java
@@ -1,10 +1,12 @@
 package com.bl.Ordermanagement.services;
 
+import com.bl.core.model.BlSerialProductModel;
 import de.hybris.platform.core.model.order.AbstractOrderEntryModel;
 import de.hybris.platform.ordersplitting.model.ConsignmentEntryModel;
 import de.hybris.platform.ordersplitting.model.ConsignmentModel;
 import de.hybris.platform.warehousing.allocation.AllocationService;
 import de.hybris.platform.warehousing.data.sourcing.SourcingResult;
+import java.util.Set;
 
 /**
  * Allocation service.
@@ -18,9 +20,17 @@ public interface BlAllocationService extends AllocationService {
    * @param orderEntry the order entry
    * @param quantity the quantity
    * @param consignment the consignment
-   * @param result
+   * @param result the sourcing result
    * @return consignment entry
    */
   public ConsignmentEntryModel createConsignmentEntry(final AbstractOrderEntryModel orderEntry,
       Long quantity, final ConsignmentModel consignment, final SourcingResult result);
+
+  /**
+   * It sets seral codes to billing charges
+   * @param consignmentEntry the consignment entry
+   * @param serialProductModels the serialproduct instances
+   */
+  public void setSerialCodesToBillingCharges(final ConsignmentEntryModel consignmentEntry,
+      final Set<BlSerialProductModel> serialProductModels);
 }
