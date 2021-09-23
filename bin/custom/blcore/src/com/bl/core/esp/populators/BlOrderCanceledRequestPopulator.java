0faa40507edb890f25f33f08e114d2dd1b656234
PLM-AVANI\Administrator <avanipatel@pragiti.com>
2021-09-23 16:12:21 +0530

    no message

--- bin/custom/blcore/src/com/bl/core/esp/populators/BlOrderCanceledRequestPopulator.java
+++ bin/custom/blcore/src/com/bl/core/esp/populators/BlOrderCanceledRequestPopulator.java
@@ -20,7 +20,7 @@ import org.springframework.util.Assert;
 
 /**
  * @author Avani Patel
- *
+ * BlOrderCanceledRequestPopulator
  */
 public class BlOrderCanceledRequestPopulator extends ESPEventCommonPopulator<OrderModel, OrderCanceledEventRequest>
 {
