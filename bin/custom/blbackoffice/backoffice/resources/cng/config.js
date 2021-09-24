86f927276c6a7a9cb6a9f79faff63fdef3340f8b
ritika_pragiti7 <rkohli@dminc.com>
2021-09-24 18:47:12 +0530

    [BL-1306]: Added changes to config file

--- bin/custom/blbackoffice/backoffice/resources/cng/config.js
+++ bin/custom/blbackoffice/backoffice/resources/cng/config.js
@@ -1,15 +1,22 @@
 CKEDITOR.editorConfig = function(config) {
 
 	config.toolbar = [
-		{ name: 'styles', items: [ 'Format' ] },
-		{ name: 'basicstyles', items: [ 'Bold', 'Italic' ] },
-		{ name: 'links', items: [ 'Link' ] },
-		{ name: 'paragraph', items: [ 'BulletedList' ] },
-		{ name: 'document', items: [ 'Source' ] },
-		{ name: 'clipboard', items: [ 'Undo', 'Redo' ] },
-		{ name: 'insert', items: [ 'Image', 'Table' ] }
+		{ name: 'document', items: [ 'Source', '-', 'Save', 'NewPage', 'ExportPdf', 'Preview', 'Print', '-', 'Templates' ] },
+		{ name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+		{ name: 'editing', items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+		{ name: 'forms', items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
+		'/',
+		{ name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat' ] },
+		{ name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
+		{ name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
+		{ name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
+		'/',
+		{ name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+		{ name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+		{ name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
+		{ name: 'about', items: [ 'About' ] }
 	];
-
+};
 	config.contentsCss = 'widgetClasspathResource/widgets/editors/wysiwygEditor/validationstyles.css';
 
 	// replace part of CKEditor base path to allow load skin from wysiwyg widget directory
