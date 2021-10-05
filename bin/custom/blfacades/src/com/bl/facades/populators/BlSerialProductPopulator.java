5fe4dbcfc6ffe84a3099db38e16489afc945474f
ritika_pragiti7 <rkohli@dminc.com>
2021-10-05 15:51:28 +0530

    [1164]: Added Removed condition

--- bin/custom/blfacades/src/com/bl/facades/populators/BlSerialProductPopulator.java
+++ bin/custom/blfacades/src/com/bl/facades/populators/BlSerialProductPopulator.java
@@ -130,7 +130,8 @@ public class BlSerialProductPopulator extends AbstractBlProductPopulator impleme
 	 */
 	private boolean serialHasActiveStatus(final BlSerialProductModel blSerialProduct) {
 
-		return SerialStatusEnum.ACTIVE.equals(blSerialProduct.getSerialStatus()) || SerialStatusEnum.ADDED_TO_CART.equals(blSerialProduct.getSerialStatus()) ;
+		return SerialStatusEnum.ACTIVE.equals(blSerialProduct.getSerialStatus()) || SerialStatusEnum.ADDED_TO_CART.equals(blSerialProduct.getSerialStatus()) ||
+				SerialStatusEnum.RECEIVED_OR_RETURNED.equals(blSerialProduct.getSerialStatus());
 	}
 
 
