325414529fd651ee515691cc62eb02e9484d7d8e
Neeraj Singh <neerajs@pragiti.com>
2021-10-06 18:09:50 +0530

    [BL-1374] Code checked-in for Exception Extra Item Event trigger point.

--- bin/custom/blcore/src/com/bl/core/esp/populators/BlExtraItemRequestPopulator.java
+++ bin/custom/blcore/src/com/bl/core/esp/populators/BlExtraItemRequestPopulator.java
@@ -1,12 +1,15 @@
 package com.bl.core.esp.populators;
 
 import com.bl.core.constants.BlCoreConstants;
+import com.bl.core.enums.NotesEnum;
 import com.bl.esp.dto.extraItem.OrderExtraItemRequest;
 import com.bl.esp.dto.extraItem.data.OrderExtraItemData;
 import de.hybris.platform.core.model.order.OrderModel;
 import de.hybris.platform.core.model.user.UserModel;
 import de.hybris.platform.servicelayer.dto.converter.ConversionException;
+import java.text.SimpleDateFormat;
 import java.util.Objects;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.Assert;
 
@@ -54,9 +57,29 @@ public class BlExtraItemRequestPopulator extends ESPEventCommonPopulator<OrderMo
     populateCommonData(orderModel , orderExtraItemData);
     orderExtraItemData.setOldorderid(StringUtils.EMPTY);
     orderExtraItemData.setTemplate(getRequestValue(getConfigurationService().getConfiguration().getString(BlCoreConstants.ORDER_EXTRA_ITEM_EVENT_TEMPLATE)));
-    orderExtraItemData.setItems(orderModel.getEntries().get(0).getProduct().getCode()); // TODO Setting dummy value, once we got the actual value then set actual value one
+    final SimpleDateFormat formatter = new SimpleDateFormat(BlCoreConstants.DATE_PATTERN);
+    orderExtraItemData.setDateplaced(formatter.format(orderModel.getDate()));
+    orderExtraItemData.setItems(getCustomerOrderNotes(orderModel));
     orderExtraItemData.setReturnfee(String.valueOf(100.00)); // TODO Setting dummy value, once we got the actual value then set actual value one
     orderExtraItemRequest.setData(orderExtraItemData);
 
+  }
+
+  /**
+   * This method is to get the customer order notes from order model
+   * @param orderModel ordermodel
+   * @return values to set on request
+   */
+  protected String getCustomerOrderNotes(final OrderModel orderModel) {
+    final StringBuilder orderNotes = new StringBuilder();
+
+    if(CollectionUtils.isNotEmpty(orderModel.getOrderNotes())){
+      orderModel.getOrderNotes().forEach(notesModel -> {
+        if(NotesEnum.CUSTOMER_OWNED_ITEMS_NOTES.getCode().equalsIgnoreCase(notesModel.getType().getCode())) {
+          orderNotes.append(StringUtils.EMPTY + notesModel.getNote());
+        }
+      });
+    }
+    return orderNotes.toString();
   }
 }
