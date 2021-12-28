51a1f072e5795d31158c979f3b14c106a8d64c54
ritika_pragiti7 <rkohli@dminc.com>
2021-12-28 22:20:03 +0530

    [BL-1436]: Added changes for configurations and renderer

---
+++ bin/custom/blbackoffice/backoffice/src/com/bl/backoffice/renderer/BlInventoryCycleCountProductNameRenderer.java
@@ -0,0 +1,42 @@
+package com.bl.backoffice.renderer;
+
+import com.bl.core.model.BlInventoryCycleCountProductDetailsModel;
+import com.hybris.cockpitng.core.config.impl.jaxb.listview.ListColumn;
+import com.hybris.cockpitng.dataaccess.facades.type.DataType;
+import com.hybris.cockpitng.engine.WidgetInstanceManager;
+import com.hybris.cockpitng.widgets.common.WidgetComponentRenderer;
+import org.apache.commons.lang.StringUtils;
+import org.zkoss.zhtml.Text;
+import org.zkoss.zul.Listcell;
+
+/**
+ * This classs is created to render product name from BlProduct
+ * @author Ritika
+ */
+public class BlInventoryCycleCountProductNameRenderer implements WidgetComponentRenderer<Listcell, ListColumn, Object> {
+
+
+    /**
+     * Renders the localized name Value
+     */
+    @Override
+    public void render(final Listcell listcell, final ListColumn lictColumn, final Object data, final DataType dataType,
+        final WidgetInstanceManager widgetInstanceManager){
+
+      String localizedName = StringUtils.EMPTY;
+      if (data instanceof BlInventoryCycleCountProductDetailsModel) {
+        final BlInventoryCycleCountProductDetailsModel productDetailsModel = (BlInventoryCycleCountProductDetailsModel) data;
+        localizedName = productDetailsModel.getInventoryCycleCountProduct().getName();
+        if(StringUtils.isBlank(localizedName)){
+          localizedName = productDetailsModel.getInventoryCycleCountProduct().getDisplayName();
+        }
+      }
+      final Text nameText = new Text();
+      nameText.setValue(localizedName);
+      listcell.appendChild(nameText);
+    }
+
+  }
+
+
+
