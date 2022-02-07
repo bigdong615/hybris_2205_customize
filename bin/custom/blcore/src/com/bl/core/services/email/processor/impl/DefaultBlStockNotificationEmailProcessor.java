a1e174e7929c1f51986dce58849d8bac21065bdb
vijayvv7060 <vvishwakarma@dminc.com>
2022-02-07 13:19:27 +0530

    no message

--- bin/custom/blcore/src/com/bl/core/services/email/processor/impl/DefaultBlStockNotificationEmailProcessor.java
+++ bin/custom/blcore/src/com/bl/core/services/email/processor/impl/DefaultBlStockNotificationEmailProcessor.java
@@ -6,12 +6,14 @@ import com.bl.esp.dto.notify.data.NotifyMeEmailRequestData;
 import de.hybris.platform.acceleratorservices.urlresolver.SiteBaseUrlResolutionService;
 import de.hybris.platform.commercefacades.product.ProductFacade;
 import de.hybris.platform.commercefacades.product.ProductOption;
+import de.hybris.platform.commercefacades.product.converters.populator.ProductPrimaryImagePopulator;
 import de.hybris.platform.commercefacades.product.data.ImageData;
 import de.hybris.platform.commercefacades.product.data.ImageDataType;
 import de.hybris.platform.commercefacades.product.data.ProductData;
 import de.hybris.platform.core.model.ItemModel;
 import de.hybris.platform.core.model.product.ProductModel;
 import de.hybris.platform.core.model.user.CustomerModel;
+import de.hybris.platform.servicelayer.dto.converter.Converter;
 import de.hybris.platform.site.BaseSiteService;
 import de.hybris.platform.stocknotificationservices.constants.StocknotificationservicesConstants;
 import de.hybris.platform.stocknotificationservices.email.processor.impl.DefaultStockNotificationEmailProcessor;
@@ -19,6 +21,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import javax.annotation.Resource;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 
@@ -30,14 +33,21 @@ public class DefaultBlStockNotificationEmailProcessor extends
   private BaseSiteService baseSiteService;
   private ProductFacade productFacade;
 
+  @Resource(name = "productConverter")
+  private Converter<ProductModel, ProductData> productConverter;
+
+  @Resource(name = "productPrimaryImagePopulator")
+  ProductPrimaryImagePopulator populator;
+
   @Override
   public void process(final CustomerModel customer, final Map<String, ? extends ItemModel> dataMap)
   {
     final ProductModel product = (ProductModel) dataMap.get(StocknotificationservicesConstants.PRODUCT);
 
-    final ProductData productData = productFacade.getProductForCodeAndOptions(product.getCode(),
-        Arrays.asList(ProductOption.BASIC, ProductOption.IMAGES));
-
+    /*final ProductData productData = productFacade.getProductForCodeAndOptions(product.getCode(),
+        Arrays.asList(ProductOption.BASIC, ProductOption.IMAGES));*/
+    final ProductData productData = new ProductData();//productConverter.convert(product);
+    populator.populate(product,productData);
     final List<ImageData> thumbnail = productData.getImages().stream()
         .filter(imageData ->
             (imageData.getImageType().equals( ImageDataType.PRIMARY)
@@ -45,8 +55,8 @@ public class DefaultBlStockNotificationEmailProcessor extends
         ).collect(Collectors.toList());
     final NotifyMeEmailRequestData emailRequestData = new NotifyMeEmailRequestData();
     emailRequestData.setEmailAddress(customer.getUid());
-    emailRequestData.setProductName(productData.getName());
-    emailRequestData.setProductUrl(getRequestedURl(BlCoreConstants.RENTAL_PDP_URL_PREFIX +productData.getCode()));
+    emailRequestData.setProductName(product.getName());
+    emailRequestData.setProductUrl(getRequestedURl(BlCoreConstants.RENTAL_PDP_URL_PREFIX +product.getCode()));
     if (CollectionUtils.isEmpty(thumbnail)){
       emailRequestData.setProductThumbURL(StringUtils.EMPTY);
     }else{emailRequestData.setProductThumbURL(thumbnail.get(0).getUrl());}
