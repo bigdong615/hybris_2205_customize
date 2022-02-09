8b645025b8ba26ed1581f6e157e2fea2d564f4bd
Manikandan <mnataraj@dminc.com>
2022-02-09 14:16:13 +0530

    changes for gift card login

--- bin/custom/blstorefront/web/webroot/WEB-INF/_ui-src/responsive/lib/ybase-0.1.0/js/blcustom.js
+++ bin/custom/blstorefront/web/webroot/WEB-INF/_ui-src/responsive/lib/ybase-0.1.0/js/blcustom.js
@@ -1035,7 +1035,7 @@ function startUsedGearCartTimer() {
 
 	function onGiftCardCloseModal()
 	{
-		$("#closeGiftCardModal , #cancelGiftCardModal").on("click", function(event){
+		$("#closeGiftCardModal , #cancelGiftCardModal ,#closeGiftCardModelWhenMixedCartError").on("click", function(event){
 			event.preventDefault();
 			var doReload = $("#doReload").val();
 			if(doReload === 'true')
