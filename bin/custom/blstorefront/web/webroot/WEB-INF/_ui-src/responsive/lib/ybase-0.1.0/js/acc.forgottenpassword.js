af20130c99b42b6809a05f17e17b2dea905ebf71
Moumita Patra <mpatra@dminc.com>
2022-04-22 09:35:14 -0400

    Changes for BL-2095

--- bin/custom/blstorefront/web/webroot/WEB-INF/_ui-src/responsive/lib/ybase-0.1.0/js/acc.forgottenpassword.js
+++ bin/custom/blstorefront/web/webroot/WEB-INF/_ui-src/responsive/lib/ybase-0.1.0/js/acc.forgottenpassword.js
@@ -9,12 +9,14 @@ ACC.forgottenpassword = {
     	$(document).on("click", ".js-forgot-password", function (e) {
     		e.preventDefault();
     		$('#forgotPass').html("");
+    		var username = $('#j_username').val();
     		$.ajax({
     			url: $(this).data("link"),
     			success: function (result) {
     				$('#forgotPass').html(result);
     				setTimeout(function () {
     					$("#forgotPass").modal('show');
+    					$("input[name='email']").val(username);
     				}, 500)
     			}
     		})
