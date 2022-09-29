55ccfca3c541ee25768ea9169d126918b597b1ff
Kumar <kvarikuntla@cognitus.com>
2022-09-29 14:28:17 +0530

    Occ Initial commit

---
+++ bin/custom/blocc/src/com/bl/occ/constants/BloccConstants.java
@@ -0,0 +1,14 @@
+package com.bl.occ.constants;
+
+@SuppressWarnings({"deprecation","squid:CallToDeprecatedMethod"})
+public class BloccConstants extends GeneratedBloccConstants
+{
+	public static final String EXTENSIONNAME = "blocc";
+	
+	private BloccConstants()
+	{
+		//empty
+	}
+	
+	
+}
