ecf9a95b4e5712754677c498f6b39f9c9e9e8576
Ravinder Reddy <>
2022-10-12 22:44:29 +0530

    Added missed file

---
+++ bin/custom/blbackoffice/backoffice/src/com/bl/backoffice/wizards/util/BulkReceiveScanToolData.java
@@ -0,0 +1,27 @@
+/**
+ *
+ */
+package com.bl.backoffice.wizards.util;
+
+import java.util.List;
+
+
+/**
+ * @author ravinder
+ *
+ */
+public class BulkReceiveScanToolData
+{
+	private List<String> barcodeInputField;
+
+	public List<String> getBarcodeInputField()
+	{
+		return barcodeInputField;
+	}
+
+	public void setBarcodeInputField(final List<String> barcodeInputField)
+	{
+		this.barcodeInputField = barcodeInputField;
+	}
+
+}
