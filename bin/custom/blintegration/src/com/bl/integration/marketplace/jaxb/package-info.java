96e1c5a9af56a53c8e58f1ff9a9b94a4476a0e92
Sravan <v-sreddypallamreddy@shutterfly.com>
2022-10-19 23:08:08 +0530

    code changes for Product Feed For Google Merchant Centre RSS tag

---
+++ bin/custom/blintegration/src/com/bl/integration/marketplace/jaxb/package-info.java
@@ -0,0 +1,7 @@
+@XmlSchema(namespace = "", elementFormDefault = XmlNsForm.QUALIFIED, xmlns =
+{ @XmlNs(prefix = "g", namespaceURI = "http://base.google.com/ns/1.0") })
+package com.bl.integration.marketplace.jaxb;
+
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
