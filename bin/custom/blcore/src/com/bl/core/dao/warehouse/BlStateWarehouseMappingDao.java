cd6625c15aa0252623539d937e43d8ddd893ac11
naveensen <nasen@dminc.com>
2023-02-14 16:52:29 +0530

    BLS-39 : 3 Day - Back Office: Allocation Logic Committing code changes to get warehouse using postal codes instead of state mapping table.

--- bin/custom/blcore/src/com/bl/core/dao/warehouse/BlStateWarehouseMappingDao.java
+++ bin/custom/blcore/src/com/bl/core/dao/warehouse/BlStateWarehouseMappingDao.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.bl.core.model.BlStateWarehouseMappingModel;
 import com.bl.core.model.BoxSizesModel;
+import com.bl.core.model.ShippingOptimizationModel;
 
 
 /**
@@ -29,4 +30,8 @@ public interface BlStateWarehouseMappingDao
 	 * @return
 	 */
 	List<BoxSizesModel> getBoxDimensions();
+
+
+	List<ShippingOptimizationModel> getWarehouseForPostalCode(final String postalCode, final Integer carrierID);
+
 }
