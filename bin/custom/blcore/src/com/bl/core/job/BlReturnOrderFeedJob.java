5d10a5096dcfd71eab3239d889d1014293da510f
HimanshuK <hkharkwal@dminc.com>
2023-05-31 17:21:35 +0530

    [BLS-331] - Return reminder email not being sent due to FTP credential issues

--- bin/custom/blcore/src/com/bl/core/job/BlReturnOrderFeedJob.java
+++ bin/custom/blcore/src/com/bl/core/job/BlReturnOrderFeedJob.java
@@ -156,8 +156,7 @@ public class BlReturnOrderFeedJob extends AbstractJobPerformable<CronJobModel>
 	private File getFile()
 	{
 		final String logFileName = new SimpleDateFormat(BlespintegrationConstants.FILE_FORMAT).format(new Date());
-		final String fileName = new StringBuilder(BlespintegrationConstants.RETUNR_ORDER_FILE_NAME_PREFIX).append(logFileName)
-				.append(BlespintegrationConstants.RETURN_ORDER_FILE_SUFFIX).toString();
+		final String fileName = new StringBuilder(BlespintegrationConstants.RETUNR_ORDER_FILE_NAME_PREFIX).append(BlespintegrationConstants.RETURN_ORDER_FILE_SUFFIX).toString();
 		final String path = Config.getParameter(BlespintegrationConstants.LOCAL_FTP_PATH);
 		createDirectoryForFTPFeed(path);
 		return new File(new StringBuilder(path).append(BlespintegrationConstants.SLASH).append(fileName).toString());
