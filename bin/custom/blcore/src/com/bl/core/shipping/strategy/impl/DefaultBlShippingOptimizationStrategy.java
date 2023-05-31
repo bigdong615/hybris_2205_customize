43821890667f2612d7387da6ad7df043853c7dcf
naveensen <nasen@dminc.com>
2023-05-31 20:42:19 +0530

    BLS-311: Optimized Start/End dates are not always calculating as expected Committing code changes to fix this issue.

--- bin/custom/blcore/src/com/bl/core/shipping/strategy/impl/DefaultBlShippingOptimizationStrategy.java
+++ bin/custom/blcore/src/com/bl/core/shipping/strategy/impl/DefaultBlShippingOptimizationStrategy.java
@@ -87,6 +87,8 @@ public class DefaultBlShippingOptimizationStrategy extends AbstractBusinessServi
                 BlDateTimeUtils.getCurrentDateUsingCalendar(BlDeliveryModeLoggingConstants.ZONE_PST, new Date()),
                 BlDeliveryModeLoggingConstants.RENTAL_DATE_PATTERN), rentalStart, sourcingLocation.getWarehouse().getCutOffTime());
 
+        BlLogger.logFormatMessageInfo(LOG, Level.DEBUG, "getUpdatedSourcingLocation : BusinessDaysDifferenceWithCutOffTime : " + result +" Order : " + order.getCode());
+
         if (result >= BlInventoryScanLoggingConstants.THREE) {
             final Map<String, Long> allocatedMap = sourcingLocation.getAllocatedMap();
             if (allocatedMap != null) {
@@ -275,10 +277,18 @@ public class DefaultBlShippingOptimizationStrategy extends AbstractBusinessServi
         
      	  // To get the INBOUND and OUTBOUND service days from shipping optimization records
         shippingOptimizationModels = getZoneDeliveryModeService().updatePreAndPostServiceDays(shippingOptimizationModels, preDaysToDeduct, postDaysToAdd);
- 
+
+
+/*      BLS-311 : Commenting below lines to fix cutoff time issue
         final int result = BlDateTimeUtils.getBusinessDaysDifferenceWithCutOffTime(consignmentModel.getOrder().getActualRentalStartDate(),
-      	   	 consignmentModel.getOrder().getRentalStartDate(), consignmentModel.getWarehouse().getCutOffTime());
-        
+      	   	 consignmentModel.getOrder().getRentalStartDate(), consignmentModel.getWarehouse().getCutOffTime());*/
+
+        final int result = BlDateTimeUtils.getBusinessDaysDifferenceWithCutOffTime(BlDateTimeUtils.convertStringDateToDate(
+                BlDateTimeUtils.getCurrentDateUsingCalendar(BlDeliveryModeLoggingConstants.ZONE_PST, new Date()),
+                BlDeliveryModeLoggingConstants.RENTAL_DATE_PATTERN), consignmentModel.getOrder().getRentalStartDate(), consignmentModel.getWarehouse().getCutOffTime());
+
+        BlLogger.logFormatMessageInfo(LOG, Level.DEBUG, "getOptimizedShippingMethodForOrder : BusinessDaysDifferenceWithCutOffTime :" + result+" Order : " + consignmentModel.getOrder().getCode());
+
         final List<Date> blackOutDates = getBlDatePickerService().getAllBlackoutDatesForGivenType(BlackoutDateTypeEnum.HOLIDAY);
 
         if(CollectionUtils.isNotEmpty(shippingOptimizationModels))
@@ -496,10 +506,13 @@ public class DefaultBlShippingOptimizationStrategy extends AbstractBusinessServi
         consignmentModel.setOptimizedShippingStartDate(optimizedStartDate);
         consignmentModel.setOptimizedShippingEndDate(optimizedEndDate);
 
-        if(null!=consignmentModel) {
-      	  updateActualRentalDatesOnOrder(consignmentModel);      	  
+        if(null != consignmentModel && null != consignmentModel.getOrder() &&
+                null != consignmentModel.getOrder().getActualRentalEndDate() &&
+                !(consignmentModel.getOrder().getActualRentalEndDate().equals(consignmentModel.getOptimizedShippingEndDate()))) {
+
+            consignmentModel.setOptimizedShippingEndDate(consignmentModel.getOrder().getActualRentalEndDate());
         }
-        
+
         final OptimizedShippingTypeEnum optimizedShippingType = checkConsignmentShippingType(consignmentModel, result);
         consignmentModel.setOptimizedShippingMethodType(optimizedShippingType);
         if (result != BlInventoryScanLoggingConstants.ZERO && optimizedShippingType != null) {
@@ -518,22 +531,6 @@ public class DefaultBlShippingOptimizationStrategy extends AbstractBusinessServi
         getModelService().refresh(consignmentModel);
     }
 
-    /**
-	 * @param consignmentModel
-	 */
- 	private void updateActualRentalDatesOnOrder(final ConsignmentModel consignmentModel)
- 	{
- 		final AbstractOrderModel orderModel = consignmentModel.getOrder();
-
- 		if (!orderModel.getActualRentalEndDate().equals(consignmentModel.getOptimizedShippingEndDate()))
-
- 		{
- 			orderModel.setActualRentalStartDate(consignmentModel.getOptimizedShippingStartDate());
- 			orderModel.setActualRentalEndDate(consignmentModel.getOptimizedShippingEndDate());
- 			getModelService().save(orderModel);
- 			getModelService().refresh(orderModel);
- 		}
- 	}
 
 	/**
      * This method will
