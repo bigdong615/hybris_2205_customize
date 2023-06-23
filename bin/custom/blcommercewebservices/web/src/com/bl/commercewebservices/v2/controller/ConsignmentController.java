6cec4de739080608af9d2d8ed157ce111e224317
Kumar <kvarikuntla@cognitus.com>
2023-06-23 11:46:21 +0530

    BLS-13: build error fix for hashmap

--- bin/custom/blcommercewebservices/web/src/com/bl/commercewebservices/v2/controller/ConsignmentController.java
+++ bin/custom/blcommercewebservices/web/src/com/bl/commercewebservices/v2/controller/ConsignmentController.java
@@ -15,6 +15,7 @@ import de.hybris.platform.webservicescommons.cache.CacheControlDirective;
 import de.hybris.platform.webservicescommons.swagger.ApiBaseSiteIdAndUserIdParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
