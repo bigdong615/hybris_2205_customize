0baf6d44466c14a3f505a0263628236039c10ad4
HimanshuK <hkharkwal@dminc.com>
2023-09-11 16:57:03 +0530

    [BLS-432] - Add tool to scan tracking code and look up order for prio gear intital commit

--- bin/custom/blbackoffice/backoffice/src/com/bl/backoffice/widget/controller/BlInboundTrackingScanController.java
+++ bin/custom/blbackoffice/backoffice/src/com/bl/backoffice/widget/controller/BlInboundTrackingScanController.java
@@ -1,7 +1,9 @@
 package com.bl.backoffice.widget.controller;
 
+import com.bl.backoffice.wizards.util.OrderTrackingScanToolData;
 import com.bl.constants.BlInventoryScanLoggingConstants;
 import com.bl.core.inventory.scan.dao.BlInventoryScanToolDao;
+import com.bl.core.model.BlSerialProductModel;
 import com.hybris.cockpitng.annotations.SocketEvent;
 import com.hybris.cockpitng.annotations.ViewEvent;
 import com.hybris.cockpitng.util.DefaultWidgetController;
@@ -9,10 +11,11 @@ import de.hybris.platform.core.model.order.OrderModel;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
-import org.zkoss.zul.Messagebox;
-import org.zkoss.zul.Textbox;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.*;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BlInboundTrackingScanController extends DefaultWidgetController {
@@ -26,12 +29,17 @@ public class BlInboundTrackingScanController extends DefaultWidgetController {
 
     @Resource(name = "blInventoryScanToolDao")
     BlInventoryScanToolDao blInventoryScanToolDao;
+
+    private Grid inboundTrackingScanToolData;
+    @Wire
+    private Div inboundTrackingSacntoolDataHeader;
     @SocketEvent(socketId = IN_SOCKET)
     public void initLoadPage(final Object inputObject)
     {
         //select {o.pk} from {Order as o JOIN Consignment as co on {co.order}={o.pk} JOIN PackagingInfo as pi on {pi.consignment} = {co.pk}} where {pi.inboundTrackingNumber} = '1Z19E5969094203950'
         this.getWidgetInstanceManager()
                 .setTitle(String.valueOf(this.getWidgetInstanceManager().getLabel("blbackoffice.order.consolidation.heading")));
+        this.inboundTrackingSacntoolDataHeader.setStyle("resize:none;display:none");
 
     }
 
@@ -46,19 +54,53 @@ public class BlInboundTrackingScanController extends DefaultWidgetController {
     {
         if (StringUtils.isBlank(textInput.getText()))
         {
+            this.inboundTrackingSacntoolDataHeader.setStyle("resize:none;display:none");
             Messagebox.show("No Tracking Number to Scan");
         }
         else{
            final List<OrderModel> orderModels =  getBlInventoryScanToolDao().getOrdersWithTrackingNo(textInput.getText().trim());
+           List<OrderTrackingScanToolData> orderTrackingScanToolData = new ArrayList<>();
            if(CollectionUtils.isNotEmpty(orderModels)) {
                orderModels.forEach(orderModel -> {
                    LOG.info("Order No is " + orderModel.getCode());
+                   orderModel.getConsignments().forEach(consignmentModel -> {
+                       consignmentModel.getConsignmentEntries().forEach(consignmentEntryModel -> {
+                           consignmentEntryModel.getSerialProducts().forEach(blProductModel -> {
+                               if (blProductModel instanceof BlSerialProductModel) {
+                                   OrderTrackingScanToolData orderTrackingScanToolData1 = new OrderTrackingScanToolData();
+                                   orderTrackingScanToolData1.setProductName((((BlSerialProductModel) blProductModel).getBlProduct().getName()));
+                                   orderTrackingScanToolData1.setProductCode((((BlSerialProductModel) blProductModel).getBlProduct().getCode()));
+                                   orderTrackingScanToolData1.setSerialNo((((BlSerialProductModel) blProductModel).getBarcode()));
+                                   orderTrackingScanToolData1.setShipmentStartDate(consignmentEntryModel.getConsignment().getOptimizedShippingStartDate().toString());
+
+                                   orderTrackingScanToolData.add(orderTrackingScanToolData1);
+                               }
+                           });
+
+
+                       });
+                   });
                });
+               this.inboundTrackingSacntoolDataHeader.setStyle("resize:none;display:block");
+               this.getInboundTrackingScanToolData().setModel(new ListModelList<>(orderTrackingScanToolData));
+               this.getInboundTrackingScanToolData().renderAll();
+
            }
            else {
                Messagebox.show("Orders are not present with the given Tracking No "+ textInput.getText());
            }
         }
+    }
+
+    @ViewEvent(componentID = "checkSerial", eventName = BlInventoryScanLoggingConstants.ON_CLICK_EVENT)
+    public void checkSerialFutureorders()
+    {
+        Messagebox.show("checkSerial clicked");
+        playSound();
+    }
+
+    private void playSound() {
+        LOG.info("play Sound worked");
     }
 
 
@@ -67,4 +109,7 @@ public class BlInboundTrackingScanController extends DefaultWidgetController {
         return blInventoryScanToolDao;
     }
 
+    public Grid getInboundTrackingScanToolData() {
+        return inboundTrackingScanToolData;
+    }
 }
