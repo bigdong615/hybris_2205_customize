5f0adf5478f7eaa2c0b5c8dcc65870cc46d7b143
Kumar <kumar@asardigital.com>
2023-12-18 14:34:38 +0530

    BLS-464: Order feed for the power reviews

---
+++ bin/custom/blcore/src/com/bl/core/order/populators/BlCompletedOrderPopulator.java
@@ -0,0 +1,58 @@
+/**
+ *
+ */
+package com.bl.core.order.populators;
+
+import de.hybris.platform.commercefacades.order.data.OrderData;
+import de.hybris.platform.converters.Populator;
+import de.hybris.platform.core.model.order.AbstractOrderEntryModel;
+import de.hybris.platform.core.model.order.OrderModel;
+import de.hybris.platform.servicelayer.dto.converter.ConversionException;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+
+/**
+ * @author ravi
+ *
+ */
+public class BlCompletedOrderPopulator implements Populator<List<OrderModel>, List<OrderData>>
+{
+	final SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+
+
+	@Override
+	public void populate(final List<OrderModel> source, final List<OrderData> target) throws ConversionException
+	{
+		for (final OrderModel order : source)
+		{
+			for (final AbstractOrderEntryModel orderentry : order.getEntries())
+			{
+			final OrderData orderData = new OrderData();
+
+			orderData.setCode(order.getCode());
+			orderData.setPage_id(orderentry.getProduct().getCode());
+			orderData.setOrder_id(order.getCode());
+			orderData.setFirst_name(order.getUser().getName());
+			orderData.setLast_name(order.getUser().getName());
+			orderData.setEmail(order.getUser().getUid());
+			orderData.setOrder_date(formatter.format(order.getCreationtime()));
+			target.add(orderData);
+
+		}
+		}
+
+
+	}
+
+	/**
+	 * @param string
+	 * @return
+	 */
+	private Object SimpleDateFormat(final String string)
+	{
+		// XXX Auto-generated method stub
+		return null;
+	}
+}
